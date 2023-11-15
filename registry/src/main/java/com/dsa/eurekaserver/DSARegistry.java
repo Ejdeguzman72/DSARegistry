97d9860b3728e13e3742d09a877952761bd7b824
Ejdeguzman72 <ejdeguzman72@gmail.com>
2023-11-15 02:35:13 -0500

    rename files

--- registry/src/main/java/com/dsa/eurekaserver/DSARegistry.java
+++ registry/src/main/java/com/dsa/eurekaserver/DSARegistry.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaserverApplication {
+public class DSARegistry {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaserverApplication.class, args);
+		SpringApplication.run(DSARegistry.class, args);
 	}
 
 }
